846622d92b6cc3f418659cebfc3830add2d593b5
Ben Lieberman <bhlieberman93@gmail.com>
2024-03-26 07:38:39 -0600

    fixed query so that it can manage both color and count

--- src/main/app/ui/canto.cljs
+++ src/main/app/ui/canto.cljs
@@ -5,18 +5,16 @@
             [goog.object :as gobj]))
 
 (defsc Canto [this {:poem/keys [canto]
-                    :parens/keys [highlighted-count]}]
-  {:query [:poem/canto {:parens/highlighted-count (comp/get-query Parens)}]
+                    :parens/keys [highlighted-count highlighted-color]}]
+  {:query [:poem/canto :parens/highlighted-count :parens/highlighted-color]
    :ident (fn [] [:component/id :canto])
    ;; constant ident like above means there MUST be initial state...
    :initial-state {}
    :route-segment ["poem"]}
-  ;; TODO: get the transaction to update my data tree...
-  ;; DONE!
   ;; perhaps each canto could even maintain its own routing
-  ;; state so that a user could bounce around more freely
+  ;; state so that a user could bounce around more freely 
   (ui-parens
-   (comp/computed {:ui/highlighted-color :blue
+   (comp/computed {:ui/highlighted-color highlighted-color
                    :ui/highlighted-count highlighted-count}
                   {:onClick
                    (fn [_e]
@@ -27,4 +25,3 @@
                            side (gobj/get data-attrs "direction")
                            computed-f (case side "up" inc "down" dec identity)]
                        (comp/transact! this [(p/change-parens {:f computed-f})])))})))
-
