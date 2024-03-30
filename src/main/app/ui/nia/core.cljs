ee605eda7a052fa6e6729c9b9a78387100af7874
Ben Lieberman <bhlieberman93@gmail.com>
2024-03-30 17:45:28 -0600

    added semantic react components

--- src/main/app/ui/nia/core.cljs
+++ src/main/app/ui/nia/core.cljs
@@ -1,16 +1,48 @@
 (ns app.ui.nia.core
   (:require [clojure.string :as str]
+            [com.fulcrologic.fulcro.algorithms.react-interop :as react-interop]
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-            [com.fulcrologic.fulcro.dom :as dom]))
+            [com.fulcrologic.fulcro.data-fetch :as df]
+            [com.fulcrologic.fulcro.dom :as dom]
+            [com.fulcrologic.fulcro.react.hooks :as hooks]
+            ["semantic-ui-react" :refer [AccordionTitle AccordionContent
+                                         Accordion Icon]]))
+
+(def ui-accordion-title (react-interop/react-factory AccordionTitle))
+(def ui-accordion-content (react-interop/react-factory AccordionContent))
+(def ui-accordion (react-interop/react-factory Accordion))
+(def ui-icon (react-interop/react-factory Icon))
+
+(defsc CantoAccordion [_ {:keys [idx canto-number content]}]
+  {:use-hooks? true}
+  (let [[active? set-active] (hooks/use-state 0)]
+    (ui-accordion
+     {}
+     (ui-accordion-title
+      {:active (= active? idx)
+       :index idx
+       :onClick (fn [_ title-props]
+                  (let [idx (.-index title-props)
+                        curr (.-active title-props)]
+                    (set-active (if curr -1 idx))))}
+      (ui-icon {:name "dropdown"})
+      canto-number)
+     (ui-accordion-content
+      {:active (= active? idx)}
+      content))))
+
+(def ui-canto-accordion (comp/factory CantoAccordion))
 
 (defsc Footnote [_this {:footnote/keys [idx text] :as _props}]
-  {:query [:footnote/idx :footnote/text]
+  {:query [:footnote/idx :footnote/text :canto/id]
    :ident :footnote/idx
-   :initial-state (fn [{:keys [idx text]}] {:footnote/idx idx :footnote/text text})}
+   :initial-state (fn [{:keys [idx text]
+                        :canto/keys [id]}] {:footnote/idx idx
+                                            :footnote/text text
+                                            :canto/id id})}
   (dom/div
    {}
    (dom/div
-    (dom/h1 "This is footnote number: " idx)
     (dom/section
      (map dom/p (str/split-lines text))))))
 
@@ -24,10 +56,15 @@
                      :parens/text text
                      :parens/footnotes
                      [(comp/get-initial-state
+                       Footnote {:idx 1 :text nil})
+                      (comp/get-initial-state
                        Footnote {:idx 4 :text nil})]})}
   (dom/div
    (dom/h4 "Parens level: " level)
    (dom/p "Parens text: " text)
+   ;; need to find a way to programmatically determine which footnotes to load
+   (dom/button :.ui.button {:onClick (fn [e] (df/load! _this [:footnote/idx 1] Footnote))}
+               "Load Footnote 1")
    (dom/ol
     (map ui-footnote footnotes))))
 
@@ -50,7 +87,7 @@
   (dom/div
    (dom/section body)
    (dom/div
-    (dom/section (ui-parens parentheses)))))
+    (dom/section :.ui.accordion (ui-parens parentheses)))))
 
 (def ui-thesis (comp/factory Thesis {:keyfn :canto/thesis}))
 
@@ -64,14 +101,17 @@
                       Thesis
                       {:id id
                        :body "thesis body"})})}
-  (dom/div 
+  (dom/div
    :.ui.container.segment
    (dom/h3 "Canto " id)
-   (ui-thesis thesis)))
+   (ui-canto-accordion {:idx id
+                        :canto-number (str "Canto " id)
+                        :content (ui-thesis thesis)}) #_(ui-thesis thesis)))
 
 (def ui-canto (comp/factory Canto))
 
 (defsc NIA [_this {:keys [canto-i canto-ii canto-iv]}]
+  ;; at some point this should perhaps become a union query...
   {:query [{:canto-i (comp/get-query Canto)}
            {:canto-ii (comp/get-query Canto)}
            {:canto-iv (comp/get-query Canto)}]
